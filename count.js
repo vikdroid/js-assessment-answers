310909aba0beb05653fa879c8253fc7036f28fd4
Vikram Raj <vikram@LivingRoom-NoteBook.(none)>
2014-08-12 18:51:39 +0530

    Added count function in count.js

--- count.js
+++ count.js
@@ -3,20 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
-			//alert("hi");
-			var cnt = start;
-			console.log(cnt);
-			function counter(){
-				setTimeout(function(){
-						cnt++; 
-						//alert("hi1");
-						console.log(cnt);
-						if(cnt < end)
-						{
-							counter();			
-						}
-			 }, 100);
-		 }
-	}
+			var timeUp;
+			function fun () {
+				console.log(start++);
+				if (start <= end) {
+					timeUp = setTimeout(fun, 100);
+				}
+			}
+			fun();
+			return {
+				cancel : function () {
+					timeUp && clearTimeout(timeUp);
+				}
+			};
+		}
   };
 });
